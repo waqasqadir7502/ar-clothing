ffd37f8e4d02ddf811f595438b8fb99b24283c68
Monis_iqbal <monisiqbal48@gmail.com>
2025-05-22 02:47:28 +0500

    error fix

--- .next/static/chunks/app_layout_tsx_f0e4c1a2._.js
+++ .next/static/chunks/app_layout_tsx_f0e4c1a2._.js
@@ -5,12 +5,13 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_layout_tsx_f0e4c1a2._.js",
   "chunks": [
-    "static/chunks/[root of the server]__24e3754d._.css",
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_ee2be3d2._.js",
+    "static/chunks/[root of the server]__589207b6._.css",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_96372c86._.js",
     "static/chunks/node_modules_next_ce8a13d2._.js",
     "static/chunks/node_modules_@fortawesome_free-solid-svg-icons_index_mjs_6d4e8d45._.js",
+    "static/chunks/node_modules_@fortawesome_free-brands-svg-icons_index_mjs_660f2e06._.js",
     "static/chunks/node_modules_69fd3a8b._.js",
-    "static/chunks/app_ccf86506._.js"
+    "static/chunks/app_35e7952b._.js"
   ],
   "source": "dynamic"
 });
