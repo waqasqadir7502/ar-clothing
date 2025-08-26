1ff8c34c2b102bd7616525a4c0cadc3077d60703
waqas qadir <65955447+waqasqadir7502@users.noreply.github.com>
2025-08-26 16:22:03 +0500

    Page error fix

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -3,12 +3,9 @@ import { getPage } from "@/src/lib/shopify";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { page: string }
-}): Promise<Metadata> {
+export async function generateMetadata(
+  { params }: { params: { page: string } }
+): Promise<Metadata> {
   const page = await getPage(params.page);
 
   if (!page) return notFound();
@@ -24,7 +21,9 @@ export async function generateMetadata({
   };
 }
 
-export default async function Page({ params }: { params: { page: string } }) {
+export default async function Page(
+  { params }: { params: { page: string } }
+) {
   const page = await getPage(params.page);
 
   if (!page) return notFound();
@@ -34,14 +33,11 @@ export default async function Page({ params }: { params: { page: string } }) {
       <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
       <Prose className="mb-8" html={page.body as string} />
       <p className="text-sm italic">
-        {`This document was last updated on ${new Intl.DateTimeFormat(
-          undefined,
-          {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          }
-        ).format(new Date(page.updatedAt))}.`}
+        {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }).format(new Date(page.updatedAt))}.`}
       </p>
     </>
   );
